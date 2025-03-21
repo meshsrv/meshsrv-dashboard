da782aac77c38c9242c243e20f8220d30cee4af4
hoywu <wu_git@outlook.com>
2025-03-21 18:10:53 +0800

    feat: disable SSR and clear prerender routes in Nuxt configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,13 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
 
+  ssr: false,
+  hooks: {
+    'prerender:routes': ({ routes }) => {
+      routes.clear(); // Do not generate any routes (except the defaults)
+    },
+  },
+
   css: ['~/assets/css/main.css'],
   vite: {
     plugins: [tailwindcss()],
