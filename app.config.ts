eb577524091efde84e2fecf335bf146220db9da1
hoywu <wu_git@outlook.com>
2025-03-22 21:57:52 +0800

    feat: add meta information and update primary color in app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -1,7 +1,16 @@
 export default defineAppConfig({
+  meta: {
+    name: 'Meshsrv',
+    version: '1.0.0',
+    repo: 'https://github.com/meshsrv',
+    author: 'hoywu',
+    authorUrl: 'https://github.com/hoywu',
+    year: '2025',
+  },
+
   ui: {
     colors: {
-      primary: 'indigo',
+      primary: 'sky',
       neutral: 'slate',
     },
   },
