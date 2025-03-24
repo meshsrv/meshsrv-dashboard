8213e1d9c8079d6033ef5e25e997477c8707be5c
hoywu <wu_git@outlook.com>
2025-03-24 14:13:52 +0800

    feat: add login page with form validation

---
+++ components/ui/Card.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { twMerge } from 'tailwind-merge';
+
+withDefaults(
+  defineProps<{
+    cls?: string;
+  }>(),
+  {
+    cls: '',
+  }
+);
+</script>
+
+<template>
+  <div
+    :class="
+      twMerge(
+        'relative flex flex-col p-6',
+        'rounded-[calc(var(--ui-radius)*2)] bg-bg',
+        'ring ring-(--ui-border)',
+        cls
+      )
+    "
+  >
+    <slot />
+  </div>
+</template>
--- pages/login/index.vue
+++ pages/login/index.vue
@@ -1,12 +1,65 @@
 <script setup lang="ts">
+import type { FormSubmitEvent } from '@nuxt/ui';
+import { z } from 'zod';
+
 definePageMeta({
   layout: 'homepage',
   middleware: ['transition-slide'],
 });
+
+const schema = z.object({
+  username: z.string(),
+  password: z.string().min(8),
+});
+type Schema = z.output<typeof schema>;
+
+const state = reactive<Partial<Schema>>({
+  username: undefined,
+  password: undefined,
+});
+
+const toast = useToast();
+async function onSubmit(event: FormSubmitEvent<Schema>) {
+  toast.add({ title: 'Success', description: 'The form has been submitted.', color: 'success' });
+  console.log(event.data);
+}
 </script>
 
 <template>
   <main class="flex-center size-full flex-col">
-    <div />
+    <UiCard cls="w-full max-w-sm gap-2 rounded-3xl bg-bg/50 p-8 shadow">
+      <UButton
+        class="absolute top-4 left-4 z-10"
+        icon="i-material-symbols-arrow-back-rounded"
+        variant="ghost"
+        color="neutral"
+        to="/"
+      />
+
+      <UIcon name="i-material-symbols-frame-person-rounded" class="w-full text-4xl" />
+      <h1 class="text-center text-lg">{{ $t('login.title') }}</h1>
+
+      <UForm :schema="schema" :state="state" class="space-y-3" @submit="onSubmit">
+        <UFormField :label="$t('login.username')" name="username">
+          <UInput
+            v-model="state.username"
+            class="w-full"
+            :placeholder="$t('login.usernamePlaceholder')"
+            autofocus
+          />
+        </UFormField>
+
+        <UFormField :label="$t('login.password')" name="password">
+          <UInput
+            v-model="state.password"
+            class="w-full"
+            type="password"
+            :placeholder="$t('login.passwordPlaceholder')"
+          />
+        </UFormField>
+
+        <UButton type="submit" class="mt-3" block loading-auto>{{ $t('login.continue') }}</UButton>
+      </UForm>
+    </UiCard>
   </main>
 </template>
