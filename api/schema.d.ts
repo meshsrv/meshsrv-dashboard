f6321ddb01959975129b3caeb5b15c2de54a9386
hoywu <wu_git@outlook.com>
2025-04-08 16:02:24 +0800

    feat(api): update api schema

--- api/schema.d.ts
+++ api/schema.d.ts
@@ -70,6 +70,22 @@ export interface paths {
         patch?: never;
         trace?: never;
     };
+    "/user-info": {
+        parameters: {
+            query?: never;
+            header?: never;
+            path?: never;
+            cookie?: never;
+        };
+        get: operations["getUser-info"];
+        put?: never;
+        post?: never;
+        delete?: never;
+        options?: never;
+        head?: never;
+        patch?: never;
+        trace?: never;
+    };
 }
 export type webhooks = Record<string, never>;
 export interface components {
@@ -234,6 +250,57 @@ export interface operations {
                         msg: string;
                         data?: {
                             token: string;
+                        };
+                    };
+                };
+            };
+        };
+    };
+    "getUser-info": {
+        parameters: {
+            query?: never;
+            header?: never;
+            path?: never;
+            cookie?: never;
+        };
+        requestBody?: never;
+        responses: {
+            200: {
+                headers: {
+                    [name: string]: unknown;
+                };
+                content: {
+                    "application/json": {
+                        /** @default 0 */
+                        code: number;
+                        /** @default success */
+                        msg: string;
+                        data?: {
+                            id: number;
+                            username: string;
+                            nickname: string | null;
+                        };
+                    };
+                    "multipart/form-data": {
+                        /** @default 0 */
+                        code: number;
+                        /** @default success */
+                        msg: string;
+                        data?: {
+                            id: number;
+                            username: string;
+                            nickname: string | null;
+                        };
+                    };
+                    "text/plain": {
+                        /** @default 0 */
+                        code: number;
+                        /** @default success */
+                        msg: string;
+                        data?: {
+                            id: number;
+                            username: string;
+                            nickname: string | null;
                         };
                     };
                 };
